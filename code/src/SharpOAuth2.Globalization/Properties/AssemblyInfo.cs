e54fda54812705ba12a566a46003d4c599494854
Geoff Horsey <geoff@shhhinnovations.com>
2011-09-21 22:51:25 -0700

    ensuring uniform version numbers

--- code/src/SharpOAuth2.Globalization/Properties/AssemblyInfo.cs
+++ code/src/SharpOAuth2.Globalization/Properties/AssemblyInfo.cs
@@ -35,4 +35,4 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.1.0.1")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.1")]
